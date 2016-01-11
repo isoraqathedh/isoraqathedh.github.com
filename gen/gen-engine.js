ba81fc5ceb32f2ef11737e93546ebc96bb331f66
isoraqathedh <isoraqathedh.zorethan@gmail.com>
2016-01-11 20:14:30 +0000

    Merged two common files together to form a single common file

--- gen/gen-engine.js
+++ gen/gen-engine.js
@@ -43,7 +43,32 @@ var plotGenerator = {
         var variablePattern = /<([^<>]+)>/;
         // ^ Regex pattern matching variables of the form <FOO>.
         while (variablePattern.test(string)) { // some variables still found
-        matches = variablePattern.exec(string);
-        string = string.replace(matches[0],
-                                plotGenerator.expandVariable(matches[1]));}
+            matches = variablePattern.exec(string);
+            string = string.replace(matches[0],
+                                    plotGenerator.expandVariable(matches[1]));}
         return string;}};
+
+function GenPlot() {
+    document.getElementById("out").innerHTML =
+        plotGenerator.expandAllVariables("<FIRST>");
+    return false; // Prevents form from submitting.
+}
+
+function init() {
+    plotGenerator.feedData(objvocab); // Feed data.
+
+    // Add the generation behaviour for the button.
+    if (document.addEventListener) {
+        // For all major browsers, except IE 8 and earlier
+        document.getElementById("generateButton").addEventListener(
+            "click", GenPlot);}
+    else if (document.attachEvent) {
+        // For IE 8 and earlier versions
+        document.getElementById("generateButton").attachEvent(
+            "onclick", GenPlot);}}
+
+function transformArrayToObject(oldArrayFormat) {
+    var objVersion = {};
+    for (var i in oldArrayFormat) {
+        objVersion[oldArrayFormat[i][0]] = oldArrayFormat[i][1];}
+    return objVersion;}
